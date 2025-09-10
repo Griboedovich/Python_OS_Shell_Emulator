4d490f814008c2bb3ed5556d1d0dda4f7229ff73
Vlad Baranov <KrakenField@yandex.ru>
2025-09-10 20:28:51 +0300

    С нуля переписан новый парсер в функцию vfs_parser. Функция args_parser была удалена вместе со старой реализацией интерпритации данных. Функции: vfs_terminal, c_exit, c_ls, c_cd переписаны под новый парсер

--- VFS.py
+++ VFS.py
@@ -62,7 +62,10 @@ class VfsTerminal(Gtk.ApplicationWindow):
 	def vfs_history_input(self, text, text_source = "", prev_char = "\n", next_char = ""):
 
 		end_iter = self.vfs_buffer.get_end_iter()		
-		self.vfs_buffer.insert(end_iter, prev_char + text_source + text + next_char)		
+		self.vfs_buffer.insert(end_iter, prev_char + text_source + text + next_char)
+
+		print(text_source + text)
+	
 		# при вызове scroll to iter сразуц после вставки
 		# в текстовый буфер, он может ввести себя некоректно
 		# для этого мы помещаем эту функцию в очередь на исполнение
@@ -83,49 +86,140 @@ class VfsTerminal(Gtk.ApplicationWindow):
 
 	def vfs_terminal(self, command_line):
 
-		command_words = command_line.split()
+		sourse, command, args_list, error_check = self.vfs_parser(command_line)
+	
+		if error_check == "пусто":
+			self.vfs_history_input("Введена пустая строка", self.SYSTEM)			
+			return
+		elif error_check == "источник":
+			self.vfs_history_input("Неккоректный источник ввода", self.SYSTEM)			
+			return
+		elif error_check == "кавычка":
+			self.vfs_history_input("Фокусы с кавычками запрещены на территории даного терминала", self.SYSTEM)			
+			return
+		elif error_check != "":
+			self.vfs_history_input("Неизвестная ошибка, как вы вообще получили это сообщение?", self.SYSTEM)			
+			return
 
-		#print(command_words)
-
-		if (len(command_words) < 2 or (( command_words[0] + " " + command_words[1] + " " ) != self.USER)):
+		if ( sourse != self.USER):
 			self.vfs_history_input("Неккоректный источник", self.SYSTEM)
 			return
 		
-		sourse = command_words[:2]
-		command_words.pop(0)
-		command_words.pop(0)
-		
-		if len(command_words) == 0: return
+		if command == "":
+			 return
 
-		command_lenght = len(command_words)
-
-		if command_words[0] == "exit":
-			self.c_exit(command_words, command_lenght)
-		elif command_words[0] == "ls":
-			self.c_ls(command_words, command_lenght)
-		elif command_words[0] == "cd":
-			self.c_cd(command_words, command_lenght)
+		if command == "exit":
+			self.c_exit(command, args_list)
+		elif command == "ls":
+			self.c_ls(command, args_list)
+		elif command == "cd":
+			self.c_cd(command, args_list)
 		else:
-				self.vfs_history_input("Неизвестная команда: " + command_words[0], self.SYSTEM)
+				self.vfs_history_input("Неизвестная команда: " + command, self.SYSTEM)
 
-	def c_exit(self, command_list, command_lenght):
+	def vfs_parser(self, command_line):
+		sourse = ""
+		command = ""
+
+		args_list = []
+		argument = ""
+
+		error_check = ""
+
+		space_flag = False
+		sign_flag = False	
+		index = -1
+		# отсутсвие источника
+		if len(command_line) == 0:
+			error_check = "пусто"
+			return sourse,command,args_list, error_check
 		
-			if command_lenght == 1:
+		index = command_line.find(">")
+		# парсер источника
+		if index == -1:
+			error_check = "источник"
+			return sourse, command, args_list, error_check
+		elif (len(command_line) < index + 2) or (command_line [index + 1] != " "):
+			error_check = "источник"
+			return sourse, command, args_list, error_check
+		else:
+			sourse = command_line[:index + 1] + command_line[index + 1]
+		#пустой ввод
+		if len(command_line) == index + 2:
+			return sourse, command, args_list, error_check
+		# обрезаем источник
+		command_line = command_line[index + 2:]
+		# извлечение команды
+		space_flag = False
+		for i in range(len(command_line)):
+			if command_line[i] != " ":
+				space_flag = True
+				command += command_line[i]
+			elif (space_flag):
+				index = i;
+				break
+			index = i
+		#обрезаем команду
+		command_line = command_line[index:]
+		
+		# в строке только команда
+		if len(command_line) == 1:
+			return sourse, command, args_list, error_check
+
+		space_flag = False
+		sign_flag = False
+		sign_argument = False
+		for i in range(len(command_line)):
+			if command_line[i] != " " and command_line[i] != '"':
+				space_flag = True
+				argument += command_line[i]
+			elif command_line[i] == '"':
+				if (sign_flag):
+					sign_flag = False
+				else:
+					sign_argument = True
+					sign_flag = True
+			elif (sign_flag):
+				argument += " "
+			elif (space_flag):
+				if sign_argument:
+					argument = "'" + argument + "'"
+					sign_argument = False
+				args_list.append(argument)
+				argument = ""
+				space_flag = False
+		if argument != "":
+			if sign_argument:
+					argument = "'" + argument + "'"
+					sign_argument = False
+			args_list.append(argument)
+			argument = ""
+
+		#кавычка не закрылась
+		if sign_flag:
+			error_check = "кавычка"
+			return sourse, command, args_list, error_check
+	
+		return sourse, command, args_list, error_check
+
+	def c_exit(self, command, args_list):
+		
+			if len(args_list) == 0:
 				self.get_application().quit()
 			else:
-				self.vfs_history_input("Неожиданные аргументы у команды exit: " + " ".join(command_list[1:]), self.SYSTEM)
+				self.vfs_history_input("Неожиданные аргументы у команды exit: " + " ".join(args_list[0:]), self.SYSTEM)
 
-	def c_ls(self, command_list, command_lenght):
+	def c_ls(self, command, args_list):
 
 		args = ["-l", "-t", "-a"]		
 
-		if command_lenght == 1:
+		if len(args_list) == 0:
 			self.vfs_history_input("ls", self.SYSTEM)
 		else:	
 			result_line = "ls "
 			error_line = "Неверные аргументы у команды ls:"
 			error_check = False
-			for i in command_list[1:]:
+			for i in args_list:
 				if i in args:
 					result_line += " " + i
 				else:
@@ -136,11 +230,11 @@ class VfsTerminal(Gtk.ApplicationWindow):
 			else:
 				self.vfs_history_input(result_line, self.SYSTEM)
 
-	def c_cd(self, command_list, command_lenght):
+	def c_cd(self, command, args_list):
 
 		args = ["-P", "-L", "-e"]		
 
-		if command_lenght == 1:
+		if len( args_list ) == 0:
 			self.vfs_history_input("cd", self.SYSTEM)
 		else:	
 			result_line = ""
@@ -148,7 +242,7 @@ class VfsTerminal(Gtk.ApplicationWindow):
 			args_line = "cd "
 			error_line = "Неверные аргументы у команды cd:"
 			error_check = False
-			for i in command_list[1:]:
+			for i in args_list:
 				if i [0] == "-":
 					if i in args:
 						args_line += " " + i
@@ -160,41 +254,12 @@ class VfsTerminal(Gtk.ApplicationWindow):
 					error_check = True
 					break
 				else:
-					file_line = self.arg_parser(i)
-					if file_line == '"error"':
-						error_line = "| Фокусы с кавычками запрещены"
-						error_check = True
-						break
-					else:
-						file_line = " ---open--> " + file_line
+					file_line = " ---open--> " + i
 			if error_check:
 				self.vfs_history_input(error_line + " |", self.SYSTEM)
 			else:
 				result_line = args_line + file_line
 				self.vfs_history_input(result_line, self.SYSTEM)
-
-	def arg_parser(self, argument):
-
-		valid_list = []
-
-		if (argument.count("'") % 2) != 0:
-			return "error"
-		if (argument.count('"') % 2) != 0:
-			return "error"
-
-		for i in argument:
-			if i == "'" or i == '"':
-				if len(valid_list) == 0 or i != valid_list[len(valid_list) - 1]:
-					valid_list.append(i)
-				else:
-					valid_list.pop()
-		if len(valid_list) != 0:
-			return "error"
-		argument = argument.replace("'", "")
-		argument = argument.replace('"', '')
-		argument = argument.replace(" ", "_")
-
-		return argument
 
 def on_activate(app):
 	# создаём окно
