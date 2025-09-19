85dde16915c9e4275e286cfe00a014649de2224d
Vlad Baranov <KrakenField@yandex.ru>
2025-09-19 17:00:38 +0300

    Added the initial bash script start.sh running pre-installed MUSHROOM VFS scripts. Updated READMY.md .

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+python3 VFS.py VFS.xml scrypts/cd_command 
+
+python3 VFS.py VFS.xml scrypts/ls_command 
+
+python3 VFS.py VFS.xml scrypts/exit_command 
+
+python3 VFS.py VFS.xml 'scrypts/command' 
+
